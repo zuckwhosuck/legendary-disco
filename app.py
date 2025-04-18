e1059dddcefa2ca05a665f02abf341894b7458d9
Bark Zuckerberg <alotokinimi2@gmail.com>
2025-04-19 01:02:09 +0530

    app py update port

--- app.py
+++ app.py
@@ -82,4 +82,5 @@ def detect_deepfake():
   
 if __name__ == '__main__':  
     port = int(os.environ.get("PORT", 10000))  
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
